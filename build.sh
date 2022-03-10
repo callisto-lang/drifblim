bed243d2ff0452d136ac4ef61f94d098df8b32c6
neauoire <aliceffekt@gmail.com>
2022-03-09 16:02:49 -0800

    Fixed issue with create-label

--- build.sh
+++ build.sh
@@ -20,5 +20,8 @@ then
 	~/Applications/butler push bin/drifblim.rom hundredrabbits/drifblim:uxn
 fi
 
+rm -rf etc/hello.sym
+rm -rf etc/hello.rom
+
 echo "Running.."
 uxncli bin/drifblim.rom etc/hello.tal && uxncli etc/hello.rom
