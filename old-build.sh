4276dc3b589a57071dda8d1342205f1ff236c9e6
Devine Lu Linvega <aliceffekt@gmail.com>
2023-01-10 21:09:39 -0800

    Added pass2 flag

---
+++ old-build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+
+echo "Cleaning.."
+rm -rf bin
+mkdir bin
+
+if [ -e "$HOME/roms/uxnlin.rom" ]
+then
+	echo "Linting.."
+	# uxncli $HOME/roms/uxnlin.rom src/drifblim.tal
+	# uxncli $HOME/roms/uxnlin.rom etc/bh.tal
+fi
+
+uxnasm src/drifblim.tal bin/drifblim-seed.rom
+
+if [ -d "$HOME/roms" ] && [ -e bin/drifblim-seed.rom ]
+then
+	cp bin/drifblim-seed.rom $HOME/roms/drifblim.rom
+    echo "Installed in $HOME/roms" 
+fi
+
+# Running hello.tal
+
+uxncli bin/drifblim-seed.rom src/drifblim.tal bin/drifblim.rom
+uxncli bin/drifblim.rom examples/hello.tal bin/hello.rom
+uxncli bin/hello.rom
+
+# Pack
+
+uxncli bin/drifblim.rom etc/bh.tal bin/bh.rom
+uxncli bin/bh.rom bin/drifblim.rom > bin/bh.log
