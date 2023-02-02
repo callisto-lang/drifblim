077314068763770943ffdd5da078e3e1738ef7b5
Devine Lu Linvega <aliceffekt@gmail.com>
2023-02-02 11:20:47 -0800

    Removed tail-calls

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ if [ -e "$HOME/roms/uxnlin.rom" ]
 then
 	echo "Linting.."
 	uxncli $HOME/roms/uxnlin.rom src/drifblim.tal
+	uxncli $HOME/roms/uxnlin.rom src/drifloon.tal
 fi
 
 uxnasm src/drifblim.tal bin/drifblim-seed.rom
@@ -16,7 +17,7 @@ uxnasm src/drifloon.tal bin/drifloon.rom
 if [ -d "$HOME/roms" ] && [ -e bin/drifblim-seed.rom ]
 then
 	cp bin/drifblim-seed.rom $HOME/roms/drifblim.rom
-    echo "Installed in $HOME/roms" 
+    echo "Installed in $HOME/roms"
 fi
 
 # Running
