ff495994193744f9d37b5f8ebd4d4171cd46b090
Devine Lu Linvega <aliceffekt@gmail.com>
2023-05-08 09:37:14 -0700

    Added tests

---
+++ tests.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+name="drifblim"
+roms_dir=${UXN_ROMS_DIR-"$HOME/roms"}
+asm="uxnasm"
+emu="uxncli"
+lin="uxncli $roms_dir/uxnlin.rom"
+src="src/${name}.tal"
+dst="bin/${name}.rom"
+
+mkdir -p bin
+
+$asm $src $dst
+
+echo "          Duplicate &sub in label."
+$emu $dst "tests/err-dup.tal" "bin/res.rom"
+
+echo "          Reference .missing in INIT."
+$emu $dst "tests/err-ref.tal" "bin/res.rom"
+
+echo "          Number #122325 in INIT."
+$emu $dst "tests/err-num.tal" "bin/res.rom"
+
+echo "          Distance ,label in INIT."
+$emu $dst "tests/err-dis.tal" "bin/res.rom"
+
+echo "          Empty file  in INIT."
+$emu $dst "tests/err-zep.tal" "bin/res.rom"
+
+echo ">         Invalid symbol name @1234 in 1234."
+$emu $dst "tests/err-hex.tal" "bin/res.rom"
+
+echo "          Number 1234567 in INIT."
+$emu $dst "tests/err-raw.tal" "bin/res.rom"
+
