d7eb7d2fc019b65041764e06a0942618c50f21f9
Devine Lu Linvega <aliceffekt@gmail.com>
2023-05-13 20:24:08 -0700

    Added uxnbal to build.sh

--- build.sh
+++ build.sh
@@ -3,17 +3,19 @@
 set -o nounset # Fails when accessing an unset variable.
 set -o errexit # Exits if a command exits with a non-zero status.
 
+name="drifblim"
 roms_dir=${UXN_ROMS_DIR-"$HOME/roms"}
 asm="uxnasm"
 emu="uxncli"
 lin="uxncli $roms_dir/uxnlin.rom"
-name="drifblim"
+bal="uxncli $roms_dir/uxnbal.rom"
 src="src/${name}.tal"
 dst="bin/${name}.rom"
 arg="${src} ${dst}"
 
 mkdir -p bin
 case "$*" in *--lint*) $lin $src ;; esac
+case "$*" in *--bal*) $bal $src ;; esac
 $asm $src $dst
 case "$*" in *--save*) cp $dst $roms_dir ;; esac
 $asm src/drifloon.tal bin/drifloon.rom
@@ -24,5 +26,4 @@ $emu $dst examples/hello.tal bin/hello.rom
 $emu bin/hello.rom
 
 # Test interactive mode
-uxnemu "${roms_dir}/left.rom" examples/hello.tal | $emu $dst ^bin/hello.rom
-
+# uxnemu "${roms_dir}/left.rom" examples/hello.tal | $emu $dst ^bin/hello.rom
