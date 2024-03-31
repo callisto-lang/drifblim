77693feddfba0182fb2973e4ada831532cbc84e1
Devine Lu Linvega <aliceffekt@gmail.com>
2024-03-30 19:52:21 -0700

    Improved tests

--- tests.sh
+++ tests.sh
@@ -1,69 +1,89 @@
 #!/bin/sh
 
-name="drifblim"
 roms_dir=${UXN_ROMS_DIR-"$HOME/roms"}
 asm="uxnasm"
-emu="uxncli"
-lin="uxncli $roms_dir/uxnlin.rom"
-src="src/${name}.tal"
-dst="bin/${name}.rom"
-
-mkdir -p bin
-
-$asm $src $dst
 
 # Usage(missing src)
-$emu $dst
+$asm
 
 # Usage(missing dst)
-$emu $dst examples/hello.tal
+$asm examples/hello.tal
 
-echo "          File missing.tal in RESET"
-$emu $dst missing.tal "bin/res.rom"
+echo ""
+echo "== Assembling"
 
-echo "          File project.tal in RESET"
-$emu $dst "tests/err-file.tal" "bin/res.rom"
+echo ""
+echo "Source missing: missing.tal"
+$asm "missing.tal" "bin/res.rom"
 
-echo "          Duplicate &sub in label."
-$emu $dst "tests/err-dup.tal" "bin/res.rom"
+echo ""
+echo "Output empty: bin/res.rom"
+$asm "tests/err-empty.tal" "bin/res.rom"
 
-echo "          Reference .missing in RESET."
-$emu $dst "tests/err-ref.tal" "bin/res.rom"
+echo ""
+echo "Source missing: project.tal"
+$asm "tests/err-file.tal" "bin/res.rom"
 
-echo "          Number #122325 in RESET."
-$emu $dst "tests/err-lithex.tal" "bin/res.rom"
+echo ""
+echo "Label duplicate: &sub"
+$asm "tests/err-dup.tal" "bin/res.rom"
 
-echo "          Number 122325 in RESET."
-$emu $dst "tests/err-rawhex.tal" "bin/res.rom"
+echo ""
+echo "Label invalid: @1234"
+$asm "tests/err-hex.tal" "bin/res.rom"
 
-echo "          Distance ,label in RESET."
-$emu $dst "tests/err-dis.tal" "bin/res.rom"
+echo ""
+echo "Hexadecimal invalid: #122325"
+$asm "tests/err-lithex.tal" "bin/res.rom"
 
-echo "          Zero-page #12 in RESET."
-$emu $dst "tests/err-zep.tal" "bin/res.rom"
+echo ""
+echo "Hexadecimal invalid: 122325"
+$asm "tests/err-rawhex.tal" "bin/res.rom"
 
-echo "          Rewind |0100 in RESET."
-$emu $dst "tests/err-rew.tal" "bin/res.rom"
+echo ""
+echo "Padding invalid: |what"
+$asm "tests/err-pad.tal" "bin/res.rom"
 
-echo "          Symbol @1234 in 1234."
-$emu $dst "tests/err-hex.tal" "bin/res.rom"
+echo ""
+echo "Writing zero-page: #12"
+$asm "tests/err-zep.tal" "bin/res.rom"
 
-echo "          Opcode ADD2q in RESET."
-$emu $dst "tests/err-opc.tal" "bin/res.rom"
+echo ""
+echo "Writing rewind: #1234 |0100"
+$asm "tests/err-rew.tal" "bin/res.rom"
 
-echo "          Number 1234567 in RESET."
-$emu $dst "tests/err-raw.tal" "bin/res.rom"
+echo ""
+echo "== Resolving"
 
+echo ""
+echo "Label unknown: ADD2q"
+$asm "tests/err-opc.tal" "bin/res.rom"
+
+echo ""
+echo "Label unknown: .missing"
+$asm "tests/err-ref.tal" "bin/res.rom"
+
+echo ""
+echo "Relative reference too far: on-reset/back"
+$asm "tests/err-far.tal" "bin/res.rom"
+
+echo ""
+echo "== STRICT"
+
+echo ""
 echo "          Nested ( in RESET."
-$emu $dst "tests/err-com.tal" "bin/res.rom"
+$asm "tests/err-com.tal" "bin/res.rom"
 
+echo ""
 echo "          Length ab in RESET"
-$emu $dst "tests/err-length.tal" "bin/res.rom"
+$asm "tests/err-length.tal" "bin/res.rom"
 
+echo ""
 echo "          Token @token-length-30-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa in reset"
-$emu $dst "tests/err-toklen.tal" "bin/res.rom"
+$asm "tests/err-toklen.tal" "bin/res.rom"
 
+echo ""
 echo "          Sublabel &some-long-sublabel in token-length-20-aaaaaaaaaaaaaaaa"
-$emu $dst "tests/err-lablen.tal" "bin/res.rom"
-
+$asm "tests/err-lablen.tal" "bin/res.rom"
+echo ""
 
